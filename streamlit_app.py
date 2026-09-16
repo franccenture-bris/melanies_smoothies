e196870980ae71282f78f4b39644d32cf28d3b60
franccenture-bris <francesco.ji.marra@accenture.com>
2026-09-16 16:04:19 +0100

    Update Snowflake session management in streamlit_app
    Replace get_active_session with st.connection to establish a Snowflake session.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 # Import python packages
 import streamlit as st
 import os
-from snowflake.snowpark.context import get_active_session 
 from snowflake.snowpark.functions import col
 
 
@@ -11,7 +10,9 @@ st.title(f":pineapple: Customize Your Smoothie! :strawberry: {st.__version__}")
 name_on_order= st.text_input("Name on Smoothie:")
 st.write("The name of your Smoothie will be:", name_on_order)
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
