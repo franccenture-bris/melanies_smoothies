4987f9d4db31334132b3a936dd9e865bf507e4a6
franccenture-bris <francesco.ji.marra@accenture.com>
2026-09-16 17:05:57 +0100

    Change fruit name to lowercase in nutrition info display

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,8 @@ if ingredients_list:
     ingredients_string=''
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen+' '
-        st.subheader(fruit_chosen + ' Nutrition Information')
-        st.write("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        st.subheader(fruit_chosen.lower() + ' Nutrition Information')
+        st.write("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen.lower())
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
 
